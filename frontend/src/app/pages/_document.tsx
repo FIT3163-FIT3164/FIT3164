a7bf94b59ff6e9f400d644343a612e634ce5f15f
faw <fawwad30@gmail.com>
2024-06-12 19:57:24 +0800

    changes to frontend, routing on backend, attempted camera integration but failed

---
+++ frontend/src/app/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta name="description" content="" />
+          <meta name="author" content="" />
+
+          <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
